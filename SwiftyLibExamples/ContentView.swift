1d3be73bfdbe6e274fdc2035b5f89af1eaae9c68
Hiren Dhamecha <hirendhamecha92@gmail.com>
2020-08-14 11:01:17 +0530

    initial part

---
+++ SwiftyLibExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftyLibExamples
+//
+//  Created by SANSKAR on 14/08/20.
+//  Copyright © 2020 SANSKAR. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
